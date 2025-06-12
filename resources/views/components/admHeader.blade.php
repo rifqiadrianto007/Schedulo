049e6d18979f31a73d74069761aa30be2eeab243
rifqiadrianto007 <232410103042@mail.unej.ac.id>
2025-06-12 23:48:52 +0700

    code prettier

--- resources/views/components/admHeader.blade.php
+++ resources/views/components/admHeader.blade.php
@@ -1,14 +1,14 @@
-    <header class="bg-blue-600 text-white px-6 py-4 flex justify-between items-center">
-        <div class="flex items-center">
-            <img src="{{ asset('img/logo.png') }}" alt="Schedulo Logo" class="h-8 w-auto">
-            <div class="ml-2 w-6 h-6 bg-white rounded-full flex items-center justify-center">
-                <i class="fas fa-check text-primary-blue text-sm"></i>
-            </div>
+<header class="bg-blue-600 text-white px-6 py-4 flex justify-between items-center">
+    <div class="flex items-center">
+        <img src="{{ asset('img/logo.png') }}" alt="Schedulo Logo" class="h-8 w-auto">
+        <div class="ml-2 w-6 h-6 bg-white rounded-full flex items-center justify-center">
+            <i class="fas fa-check text-primary-blue text-sm"></i>
         </div>
-        <div class="flex items-center space-x-3">
-            <span class="text-lg">Admin 123</span>
-            <div class="w-10 h-10 bg-green-500 rounded-full flex items-center justify-center">
-                <i class="fas fa-user text-white"></i>
-            </div>
+    </div>
+    <div class="flex items-center space-x-3">
+        <span class="text-lg">Admin 123</span>
+        <div class="w-10 h-10 bg-green-500 rounded-full flex items-center justify-center">
+            <i class="fas fa-user text-white"></i>
         </div>
-    </header>
+    </div>
+</header>
