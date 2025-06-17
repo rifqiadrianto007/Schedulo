403d2710311ad567b75e337a248d57214d5f07f0
rifqiadrianto007 <232410103042@mail.unej.ac.id>
2025-06-17 23:08:53 +0700

    admin layouting

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8" />
     <meta content="width=device-width, initial-scale=1" name="viewport" />
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <title>Admin Panel - Schedulo</title>
+    <title>Schedulo</title>
 
     <script src="https://cdn.tailwindcss.com"></script>
     <script>
